24356168b2d55c2994194b8ef47b4622675bc263
Harrison <squad1033@gmail.com>
2024-09-10 00:38:45 +0800

    INCLUDE_DIR check and set include path

--- code/hello.c
+++ code/hello.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include "hello.h"
 
-int main() {
+int main()
+{
     // 打印 "Hello, world!" 到標準輸出
     printf("Hello, world!\n");
     return 0;
 }
-
---
+++ code/include/hello.h
@@ -0,0 +1,12 @@
+#ifndef __HELLO_H_
+#define __HELLO_H_
+
+#include <stdio.h>
+
+
+
+
+
+
+
+#endif
